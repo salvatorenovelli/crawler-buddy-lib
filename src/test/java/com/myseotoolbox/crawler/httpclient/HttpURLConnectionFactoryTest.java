8d9255f0bc01685f53f1271b6f92836461c85134
Salvatore Novelli <salvatore.novelli@gmail.com>
2022-05-21 20:13:53 +0100

    removed deprecated assertThat

--- src/test/java/com/myseotoolbox/crawler/httpclient/HttpURLConnectionFactoryTest.java
+++ src/test/java/com/myseotoolbox/crawler/httpclient/HttpURLConnectionFactoryTest.java
@@ -7,8 +7,8 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.stream.Stream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class HttpURLConnectionFactoryTest {
